e9f8adfb25fa80aaa46be9c8a339b2b3d3e32782
Logan Mancuso <logan.mancuso@scoutmotors.com>
2025-08-19 15:39:08 -0400

    stow dots

--- stow-dotfiles.sh
+++ stow-dotfiles.sh
@@ -33,9 +33,6 @@ function unstow_dotfiles() {
 # Main function to handle options
 function main() {
   echo "System Information:"
-  echo "hostname: $(cat /etc/hostname)"
-  echo "cpu: $(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1"%"}')"
-  echo "memory: $(free | grep Mem | awk '{print $3/$2 * 100.0"%"}')"
   echo "user: $(whoami)"
   echo "time: $(date)"
 
