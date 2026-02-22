e56ac0807e766fbb2f78418d7fc135b646d0a887
Logan Mancuso <mickeyacejr@live.com>
2026-02-22 10:34:38 -0500

    update

--- stow-dotfiles.sh
+++ stow-dotfiles.sh
@@ -12,7 +12,7 @@ function main() {
 
   # Use getopt for long option parsing
   OPTIONS=$(getopt -o "" --long stow,unstow -- "$@")
-  
+
   # Check if getopt succeeded
   if [[ $? -ne 0 ]]; then
     echo "Failed to parse options."
@@ -22,37 +22,37 @@ function main() {
   # Reorder options and arguments
   eval set -- "$OPTIONS"
 
-  declare -a dotfiles=("aliases" "bash" "continue" "ghostty" "git" "nvim" "ssh" "starship" "vscode" "zellij" "zoxide" "zsh")
+  declare -a dotfiles=("aliases" "bash" "continue" "ghostty" "git" "nvim" "ssh" "starship" "vscode" "zed" "zellij" "zoxide" "zsh")
 
   # Parse options
   while true; do
     case "$1" in
-      --stow)
-        echo "stowing dotfiles..."
-        for dot in "${dotfiles[@]}"; do
-          stow -v --dotfiles --adopt --target="$HOME" "$dot"
-        done
-        # leaving thses comments here to remind me how to recycle services if i add a job to the systemd folder
-        # systemctl --user daemon-reload
-        # systemctl --user enable nextcloud.service
-        # systemctl --user start nextcloud.service
-        shift
-        ;;
-      --unstow)
-        echo "Unstowing dotfiles..."
-        for dot in "${dotfiles[@]}"; do
-          stow -D -v "$dot"
-        done
-        shift
-        ;;
-      --)
-        shift
-        break
-        ;;
-      *)
-        echo "Unknown option: $1"
-        exit 1
-        ;;
+    --stow)
+      echo "stowing dotfiles..."
+      for dot in "${dotfiles[@]}"; do
+        stow -v --dotfiles --adopt --target="$HOME" "$dot"
+      done
+      # leaving thses comments here to remind me how to recycle services if i add a job to the systemd folder
+      # systemctl --user daemon-reload
+      # systemctl --user enable nextcloud.service
+      # systemctl --user start nextcloud.service
+      shift
+      ;;
+    --unstow)
+      echo "Unstowing dotfiles..."
+      for dot in "${dotfiles[@]}"; do
+        stow -D -v "$dot"
+      done
+      shift
+      ;;
+    --)
+      shift
+      break
+      ;;
+    *)
+      echo "Unknown option: $1"
+      exit 1
+      ;;
     esac
   done
 }
